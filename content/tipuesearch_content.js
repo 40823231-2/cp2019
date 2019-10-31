2c291fd48e72770d183529e760f2aaa95929d4cb
40823231-2 <40823231@gm.nfu.edu.tw>
2019-10-31 13:16:36 +0800

    add embedding dartpad

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://mde.tw/cp2019 \xa0 \n https://dartpad.dev?id=5dd80af87035d20438ead66feac45419 \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'https://mde.tw/cp2019 \xa0 \n https://dartpad.dev?id=5dd80af87035d20438ead66feac45419 \n https://dartpad.dev?id= 71482a9ed9137c9c6de05da073b45c01 \n \n', 'tags': '', 'url': 'About.html'}]};
