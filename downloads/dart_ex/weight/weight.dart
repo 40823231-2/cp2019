03e906dcc8c7bf3dcde72db7b95dfe7714b6629b
40823231-2 <40823231@gm.nfu.edu.tw>
2020-01-02 15:08:27 +0800

    更新網站

--- downloads/dart_ex/weight/weight.dart
+++ downloads/dart_ex/weight/weight.dart
@@ -1,20 +1,29 @@
-main() {
-  int len;
-  var type;
+lbtokg(num lb){
+return lb*0.454;
+}
+
+kgtolb(num kg){
+return kg*2.205;
+}
+
+main() { 
+  int len; 
+  var type;  
   var number;
-  List temp = [ "25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
-  for (var data in temp) {
-    len = data.length;
-    type = data[len-2];
-    number = data.substring(0, len-2);
+  List weight = ["25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
+  for (var data in weight) {
+    
+    len = data.length;    
+    type = data[len-2];   
+    number = data.substring(0, len-2);   
     number = int.parse(number);
-    if (type == "k"){
-      double type=number*2.2;
-      print("kg:$number, =$type lb");
-    }else{
-      double type=number*0.45;
-      print("lb:$number, =$type kg");
+   
+    if (type == "l"){     
+      print("$number 英磅 =  ${lbtokg(number)} 公斤");
+      
+    }else{     
+      print("$number 公斤 =  ${kgtolb(number)} 英磅");
     }
       
-  }
+  } 
 } 
