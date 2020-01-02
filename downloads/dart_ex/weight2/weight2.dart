67ddbfcaea0a6dbea481199ed7e027df15c4baa3
40823231-2 <40823231@gm.nfu.edu.tw>
2020-01-02 18:07:57 +0800

    17周修改

--- downloads/dart_ex/weight2/weight2.dart
+++ downloads/dart_ex/weight2/weight2.dart
@@ -1,3 +1,5 @@
+import "dart:html";
+
 lbtokg(num lb){
 return lb*0.4536;
 }
@@ -6,23 +8,29 @@ kgtolb(num kg){
 return kg*2.2046;
 }
 
-main() { 
-  int len; 
+main() {
+    
+    InputElement tempInput = querySelector("#weight");
+    querySelector("#submit").onClick.listen((e) => convert(tempInput.value));
+}
+
+convert(String data){ 
+  int len;  
   var type;  
   var number;
-  List weight = ["25kg", "30lb", "56lb", "14kg", "68lb", "198kg"];
-  for (var data in weight) {
-    
-    len = data.length;    
-    type = data[len-2];   
-    number = data.substring(0, len-2);   
-    number = int.parse(number);
-   
-    if (type == "l"){     
-      print("$number 英磅 =  ${lbtokg(number).toStringAsFixed(4)} 公斤");
-      
-    }else{     
-      print("$number 公斤 =  ${kgtolb(number).toStringAsFixed(4)} 英磅");
-    }
+  
+  LabelElement output = querySelector("#output");
+  
+  len = data.length;  
+  type = data[len-2];
+  number = data.substring(0, len-2);
+  
+  number = int.parse(number);
+  if (type == "L" || type == "l"){
+     output.innerHtml = " $number 英磅 =  ${lbtokg(number).toStringAsFixed(4)} 公斤";
+  } else if ((type == "K" || type == "k")){
+     output.innerHtml = " $number 公斤 =  ${kgtolb(number).toStringAsFixed(4)} 英磅";
+  } else {
+     output.innerHtml = "請輸入數字加上 LB 或 KG!";
   }
-  } 
+} 
